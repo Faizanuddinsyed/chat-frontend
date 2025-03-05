389ca9fdaeca180a856c0f24511af0c72df240d8
Faizan <syedf928@gmail.com>
2025-03-05 12:57:39 +0530

    Removed Material-UI and replaced with Tailwind CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 
 import { createRoot } from 'react-dom/client'
-import { CssBaseline } from '@material-ui/core'
+
 import App from './App.jsx'
 
 
 
 createRoot(document.getElementById('root')).render(
   <>
-    {/* <CssBaseline /> Reset default browser styles */}
+   
     <App />
   </>,
 )
